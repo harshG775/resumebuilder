a70a7bf6c0b0569a0876f4517f95ec3307241034
Harsh <hgaur491@gmail.com>
2023-12-06 19:52:31 +0530

    fixing layout

--- src/pages/resumeBuilder/ResumeBuilder.jsx
+++ src/pages/resumeBuilder/ResumeBuilder.jsx
@@ -13,52 +13,54 @@ import Declaration from "../../pageSections/resumeBuilder/declaration/Declaratio
 import CustomSection from "../../pageSections/resumeBuilder/customsection/CustomSection.jsx";
 
 
+const Controls = () => {
+    const { zoomIn, zoomOut, resetTransform } = useControls();
+    return (
+        <div className="absolute bottom-4 left-[10rem] right-0 p-4 z-50">
+            <div className="flex gap-4 mx-auto">
+                <div>
+                    other Controls
+                </div>
+                <ul className="grid gap-2 grid-flow-col">
+                    <li><button onClick={() => zoomIn()}>Zoom In</button></li>
+                    <li><button onClick={() => zoomOut()}>Zoom Out</button></li>
+                    <li><button onClick={() => resetTransform()}>Center</button></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
 export default function ResumeBuilder() {
-    const Controls = () => {
-        const { zoomIn, zoomOut, resetTransform } = useControls();
-        return (
-            <>
-                <button onClick={() => zoomIn()}>Zoom In</button>
-                <button onClick={() => zoomOut()}>Zoom Out</button>
-                <button onClick={() => resetTransform()}>Reset</button>
-            </>
-        );
-    };
+
     
 	return (
-        <TransformWrapper
-                initialScale={1}
-                // initialPositionX={200}
-                // initialPositionY={100}
-            >
-            <main className="grid grid-cols-[10rem_1fr_10rem]">
-                <div>
-                    <h1>left</h1>
+        <div className="grid grid-cols-[10rem_1fr]">
+            <div className="border border-gray-600">
+                <h1>Resume Builder</h1>
+            </div>
+            <main className="">
+                <TransformWrapper>
                     <Controls />
-                </div>
-                <TransformComponent>
-                    <div className="mx-auto grid place-content-center h-screen w-screen">
-                        <div className="scale-50 w-a4 h-a4 m-4 mx-auto shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+                    <TransformComponent>
+                        <div className=" mx-auto grid place-content-center h-screen w-[calc(100vw-10rem)]">
+                            <div className="scale-50 w-a4 h-a4 mx-auto shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
 
-                            <BasicInfo />
-                            <Summary />
-                            <Skills />
-                            <Education />
-                            <Experience />
-                            <Projects />
-                            <Certifications />
-                            <Languages />
-                            <Interests />
-                            <CustomSection/>
-                            <Declaration />
+                                <BasicInfo />
+                                <Summary />
+                                <Skills />
+                                <Education />
+                                <Experience />
+                                <Projects />
+                                <Certifications />
+                                <Languages />
+                                <Interests />
+                                <CustomSection/>
+                                <Declaration />
+                            </div>
                         </div>
-                    </div>
-                </TransformComponent>
-                <div>
-                    <h1>right</h1>
-                </div>
+                    </TransformComponent>
+                </TransformWrapper>
             </main>
-        </TransformWrapper>
-
+        </div>
 	);
 }
