df40456bcd60e90349b7aed30f7d434f5b867971
Harsh <hgaur491@gmail.com>
2023-12-06 18:01:23 +0530

    ///

--- src/pageSections/resumeBuilder/ResumeBuilder.jsx
+++ src/pageSections/resumeBuilder/ResumeBuilder.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import BasicInfo from "../home/basicInfo/BasicInfo";
 import Summary from "../home/summary/Summary";
 import Skills from "../home/skills/Skills";
@@ -12,19 +14,39 @@ import CustomSection from "../home/customsection/CustomSection.jsx";
 
 
 export default function ResumeBuilder() {
+    const [isLeftBarOpen, setIsLeftBarOpen] = useState(false);
+    const [isRightBarOpen, setIsRightBarOpen] = useState(false);
+    const handleLeftBar =()=>{
+        setIsLeftBarOpen(!isLeftBarOpen)
+    }
+    const handleRightBar =()=>{
+        setIsRightBarOpen(!isRightBarOpen)
+    }
 	return (
-		<>
-			<BasicInfo />
-			<Summary />
-			<Skills />
-			<Education />
-			<Experience />
-			<Projects />
-			<Certifications />
-			<Languages />
-			<Interests />
-            <CustomSection/>
-			<Declaration />
-		</>
+        <div className="grid grid-cols-[10rem_1fr_10rem]">
+            <div>
+                <button onClick={handleLeftBar}>toggle</button>
+                right side
+            </div>
+            <main className="overflow-scroll ">
+                <div className=" w-[210mm] h-[297mm] m-4 mx-auto shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+                    <BasicInfo />
+                    <Summary />
+                    <Skills />
+                    <Education />
+                    <Experience />
+                    <Projects />
+                    <Certifications />
+                    <Languages />
+                    <Interests />
+                    <CustomSection/>
+                    <Declaration />
+                </div>
+            </main>
+            <div>
+                <button onClick={handleRightBar}>toggle</button>
+                right side
+            </div>
+        </div>
 	);
 }
