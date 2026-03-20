023be9232b0da91452607c839ed5c99e24442bd3
harshG775 <hgaur491@gmail.com>
2026-03-20 16:36:49 +0530

    init tanstack start

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+    semi: false,
+    singleQuote: false,
+    trailingComma: "all",
+    tabWidth: 4,
+}
+
+export default config
