0cab983d800954c36d92c06b21bec121a0b06b0c
Harsh_G <hgaur491@gmail.com>
2026-04-18 00:30:57 +0530

    adding header and main section

--- src/routes/resumes/$resume/index.tsx
+++ src/routes/resumes/$resume/index.tsx
@@ -5,6 +5,7 @@ import { FieldGroup, FieldSeparator } from "#/components/ui/field"
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
 import { BasicsSection, ProjectsSection, SummarySection } from "./-components/Editor"
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch"
+import React from "react"
 
 export const Route = createFileRoute("/resumes/$resume/")({
     ssr: false,
@@ -89,10 +90,61 @@ function RouteComponent() {
                     }}
                 >
                     <div className="relative w-full h-full bg-white text-black shadow-lg">
-                        <main className="p-6 aspect-8.5/11 w-7xl">
-                            <h1 className="text-xl font-bold">Resume Preview</h1>
-                            <p className="text-sm text-gray-600">This maintains proper 8.5:11 aspect ratio.</p>
-                        </main>
+                        <form.Subscribe selector={(state) => state.values}>
+                            {(values) => (
+                                <div className="p-6 aspect-[8.5/11] w-7xl">
+                                    <header className="mb-4 flex">
+                                        <div className="flex-1">
+                                            <h1 className="text-2xl font-bold text-neutral-900">
+                                                {values.basics.name || "Name"}
+                                            </h1>
+                                            <p className="text-sm font-medium text-neutral-700">
+                                                {values.basics.headline}
+                                            </p>
+                                            <div className="text-[11px] mt-1 flex flex-wrap gap-2 text-neutral-600">
+                                                {values.basics.email && <span>{values.basics.email}</span>}
+                                                {values.basics.phone && <span>| {values.basics.phone}</span>}
+                                                {values.basics.location && <span>| {values.basics.location}</span>}
+                                                {values.basics.website && (
+                                                    <a
+                                                        href={values.basics.website.url}
+                                                        target="_blank"
+                                                        rel="noopener noreferrer"
+                                                        className="underline text-blue-700 hover:text-blue-800 wrap-break-word"
+                                                    >
+                                                        {values.basics.website.label}
+                                                    </a>
+                                                )}
+                                                {values.basics?.customFields.length > 0 && (
+                                                    <>
+                                                        {values.basics.customFields.map((link, i) => (
+                                                            <React.Fragment key={link.id || i}>
+                                                                {i > 0 && <span>|</span>}
+                                                                <a
+                                                                    key={link.id || i}
+                                                                    href={link.url}
+                                                                    target="_blank"
+                                                                    rel="noopener noreferrer"
+                                                                    className="underline text-blue-700 hover:text-blue-800 wrap-break-word"
+                                                                >
+                                                                    {link.label}
+                                                                </a>
+                                                            </React.Fragment>
+                                                        ))}
+                                                    </>
+                                                )}
+                                            </div>
+                                        </div>
+                                    </header>
+                                    <main>
+                                        <section></section>
+                                        <section></section>
+                                        <section></section>
+                                        <section></section>
+                                    </main>
+                                </div>
+                            )}
+                        </form.Subscribe>
                     </div>
                 </TransformComponent>
             </TransformWrapper>
