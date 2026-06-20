76e03293157f6803a96c3f288c81dcd40f96013b
Harsh_G <hgaur491@gmail.com>
2026-06-20 17:36:32 +0530

    feat(database): add resume table and update auth schema for user-resume relationship

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv"
 import { defineConfig } from "drizzle-kit"
 
-config({ path: [".env"] })
+config({ path: [".env",".env.local"] })
 
 const DATABASE_URL = process.env.DATABASE_URL
 if (!DATABASE_URL) throw new Error(".env DATABASE_URL not found")
