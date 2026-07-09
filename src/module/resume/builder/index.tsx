3427eccf96780fee27d215edbf4fa283fec1137e
Harsh_G <hgaur491@gmail.com>
2026-07-09 12:57:08 +0530

    feat: add resizable panels and zoom functionality to resume builder
    - Introduced ResizablePanelGroup, ResizablePanel, and ResizableHandle components for dynamic resizing.
    - Integrated resizable panels in the Builder component for better layout management.
    - Added zoom and pan capabilities using react-zoom-pan-pinch for enhanced user experience.
    - Updated package.json to include react-zoom-pan-pinch as a dependency.

--- src/module/resume/builder/index.tsx
+++ src/module/resume/builder/index.tsx
@@ -1,6 +1,12 @@
+import { Button } from "#/components/ui/button"
+import { FieldGroup, FieldSeparator } from "#/components/ui/field"
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "#/components/ui/resizable"
 import { useAppForm } from "#/hooks/form"
+import { Link } from "@tanstack/react-router"
 import { ResumeZodSchema } from "../schema/resume.zod-schema"
 import type { ResumeValues } from "../schema/resume.zod-schema"
+import { DownloadIcon, HomeIcon } from "lucide-react"
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch"
 
 type BuilderProps = {
     resumeValue: ResumeValues
@@ -10,5 +16,59 @@ export default function Builder({ resumeValue }: BuilderProps) {
         defaultValues: resumeValue,
         validators: { onChange: ResumeZodSchema },
     })
-    return <div>{JSON.stringify(form.state.values)}</div>
+    return (
+        <div className="h-screen w-screen bg-muted">
+            <div className="fixed inset-0 z-20 pointer-events-none">
+                <div className="bg-sidebar shadow flex items-center h-12 pointer-events-auto">
+                    <div className="flex-1">|</div>
+                    <div className="flex items-center gap-2">
+                        <Link to="/dashboard/resumes" className="flex items-center gap-2">
+                            <HomeIcon className="size-4" />
+                            <span className="sr-only">Dashboard</span>
+                        </Link>
+                        {" / "}header
+                    </div>
+                    <div className="flex-1 text-right">
+                        <Button className={""}>
+                            <DownloadIcon /> <span className="sr-only sm:not-sr-only">Download</span>
+                        </Button>
+                    </div>
+                </div>
+                <ResizablePanelGroup orientation="horizontal">
+                    <ResizablePanel minSize={"16rem"}  maxSize={"40%"} className="border pointer-events-auto bg-sidebar flex">
+                        {/* resume content editor */}
+                        <FieldGroup className="h-full overflow-y-auto scrollbar-thin p-4">
+                            {/* <BasicsSection form={form} /> */}
+                            <FieldSeparator />
+                            {/* <SummarySection form={form} /> */}
+                            <FieldSeparator />
+                            {/* <SkillsSection form={form} /> */}
+                            <FieldSeparator />
+                            {/* <ExperienceSection form={form} /> */}
+                            <FieldSeparator />
+                            {/* <ProjectsSection form={form} /> */}
+                            <FieldSeparator />
+                            {/* <EducationSection form={form} /> */}
+                            <FieldSeparator />
+                            {/* <CertificationsSection form={form} /> */}
+                        </FieldGroup>
+                    </ResizablePanel>
+                    <ResizableHandle withHandle />
+                    <ResizablePanel defaultSize={50}></ResizablePanel>
+                    <ResizableHandle withHandle />
+                    <ResizablePanel minSize={"16rem"}  maxSize={"40%"} className="border pointer-events-auto bg-sidebar"></ResizablePanel>
+                </ResizablePanelGroup>
+            </div>
+            <TransformWrapper initialScale={0.3} minScale={0.1} maxScale={2} centerOnInit limitToBounds={false}>
+                <TransformComponent
+                    wrapperStyle={{
+                        width: "100%",
+                        height: "100%",
+                    }}
+                >
+                    <div className="max-w-3xl w-full">{JSON.stringify(form.state.values)}</div>
+                </TransformComponent>
+            </TransformWrapper>
+        </div>
+    )
 }
